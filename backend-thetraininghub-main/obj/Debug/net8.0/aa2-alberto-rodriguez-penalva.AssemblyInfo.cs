0466dfc2ead6a3fd09c25cc7794fd3a587aff6ef
Alberto Rodriguez Penalva <127990307+AlbertoRodriguez4@users.noreply.github.com>
2025-07-25 18:14:42 +0200

    Cambio Puertos

--- backend-thetraininghub-main/obj/Debug/net8.0/aa2-alberto-rodriguez-penalva.AssemblyInfo.cs
+++ backend-thetraininghub-main/obj/Debug/net8.0/aa2-alberto-rodriguez-penalva.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aa2-alberto-rodriguez-penalva")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+940eaee890c4f6b611e9ebf2a8b935dd617fedb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("aa2-alberto-rodriguez-penalva")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aa2-alberto-rodriguez-penalva")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
